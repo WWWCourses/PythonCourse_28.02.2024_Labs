13367fd3e7f3dd262eb2fa3fe5e49a962f518e7e
WWWCourses <ivawebcourses@gmail.com>
2024-03-06 19:25:35 +0200

    demo for tab in github

---
+++ Lab3/assignment.py
@@ -0,0 +1,13 @@
+# buget_of_current_year = 1
+
+# # incement buget_of_current_year
+# # buget_of_current_year = buget_of_current_year + 1
+# buget_of_current_year +=  1
+# buget_of_current_year +=  1
+
+# print(buget_of_current_year)
+
+# x = 5
+# x %= 2 # x = x%2
+
+# print(x) # 1;
---
+++ Lab3/strings_opetations.py
@@ -0,0 +1,27 @@
+# strings are immutabile
+# x = '5'
+# x = 'c'
+
+# RAM:
+
+#      :0x123: '5'
+#     x:0x342: 'c'
+
+
+# name = "John's pub"
+# name = 'John\'s pub'
+
+# print(name)
+
+# text = 'Line1\nLine2\nLine3'
+# print(text)
+
+text1 = 'a\tb'
+text2 = 'a	b'
+print(text1)
+print(text2)
+
+
+# demo for tab alignment
+if 1:
+	print(1)
