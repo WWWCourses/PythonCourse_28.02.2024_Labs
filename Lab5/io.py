640f0e38a3f719f077903fe0b86297ddad937dbe
WWWCourses <ivawebcourses@gmail.com>
2024-03-13 21:04:37 +0200

    Lab5: 13.03.2024 DONE: L5: IO

---
+++ Lab5/io.py
@@ -0,0 +1,80 @@
+# ----------------------------------- input ---------------------------------- #
+# user_name = input('Enter your name:')
+# print(f'user_name:{user_name}')
+
+# x = input('x=')
+# print( type(x) )
+# x = int(x)
+# print( type(x) )
+# print(f'x*3={x*3}')
+
+# x = int(input('x='))
+# print(f'x*3={x*3}')
+
+# print(bool(-3.25))
+
+# if -3.25:
+#     print('hi')
+
+# x = input('x=')
+# res = int(x) * 2
+# print(res)
+
+# print( type(x) ) # str
+
+
+# x = int( input('x=') )
+# print( x*3 )
+
+
+# print( int('0101', base=2) )
+
+
+
+# ------------------------ Error (Exception ) Handling ----------------------- #
+# a run time error can occur:
+# try:
+#     x = int( input('x:') )
+#     print(f'x={3}')
+# except:
+#     print('I tald you x MUST BE A NUMBER!!!')
+
+
+# try:
+#     y = int( input('y:') )
+#     print(f'y={3}')
+# except:
+#     print('I tald you y MUST BE A NUMBER!!!')
+
+
+# print('END')
+
+# print( 3/0 ) # ZeroDivisionError
+# print( x )     # NameError:
+# int('pesho')    # ValueError
+
+# ------------------------- Handle multple exceptions ------------------------ #
+# try:
+#     x=int(input('x:'))
+#     print(f'x={x}')
+#     print( 3/x )
+# except ValueError:
+#     print('I tald you x MUST BE A NUMBER!!!')
+# except ZeroDivisionError:
+#     print('I can not divide by 0!')
+# except:
+#     print('Ups, something went wrong...')
+
+# ----------------------------------- print ---------------------------------- #
+# print(1,2,3)            # 1 2 3
+# print(1,2,3, sep=';')   # 1;2;3
+
+# print(1, end=" ")
+# print(2, end='alabala')
+# print(3)
+
+
+
+
+
+
