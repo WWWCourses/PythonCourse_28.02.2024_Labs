b901b95cbd7858417b84e940bd6c558634eb3c5d
WWWCourses <ivawebcourses@gmail.com>
2024-04-12 21:16:55 +0300

    Lab14: 12.04.2024 DONE: Midterm Project Assignments

---
+++ HW_Reviews/Lists/TODO_task7.py
@@ -0,0 +1,16 @@
+# ================= Task 7 ================
+# Write a program that finds the maximum number of consecutive
+# identical items in a list.
+# Example: {2, 1, 1, 2, 3, 3, 2, 2, 2, 1} -> {2, 2, 2}. '2' * counter
+
+l =  [2, 1, 1, 2, 3, 3, 2, 2, 2, 1]
+
+cons = [
+    [1,1],
+    [3,3],
+    [2,2,2]
+]
+
+for el in l:
+
+
---
+++ Lab14/tmp.py
@@ -0,0 +1,17 @@
+# def bar(*args):
+#     pass
+
+# def foo(name, age):
+#     print(name, age)
+
+# # data = ['ada', 23]
+# data = {
+#     'name':'ada',
+#     'age': 23
+# }
+
+# foo(**data)
+
+# # foo(data[0], data[1])
+# # foo(*data)
+
