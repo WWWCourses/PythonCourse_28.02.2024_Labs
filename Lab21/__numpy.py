5a2fef2ee6f735137c62790721a335bd5f714ae1
WWWCourses <ivawebcourses@gmail.com>
2024-05-15 20:28:44 +0300

    tmp

---
+++ Lab21/__numpy.py
@@ -0,0 +1,26 @@
+#  numpy.ndarray(shape, dtype=float, buffer=None, offset=0, strides=None, order=None)[source]
+
+class ndarray:
+
+    def __init__(self, shape, dtype=float, buffer=None, offset=0, strides=None, order=None):
+        self.shape = shape
+        self.dtype = dtype
+        self.T = .
+
+    def all(self):
+        pass
+
+
+
+# class Person:
+#     # instance method
+#     def __init__(self, name, age) -> None:
+#         # set instance attributes
+#         self.name = name
+#         self.age = age
+
+#     def greet(self):
+#         print(f"Hello, I'm {self.name}, {self.age} years old!")
+
+
+# pesho = Person('Pesho', 23)
---
+++ Lab21/numpy_basics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# l = [1, 2, 3]
+# a = np.array( l, dtype=np.ubyte )
+
+arr = np.array([1,2,3])
+arr.dtype
+
+
+
